0c5ae4abfbd6ba6c2bb89c730fbd3e6a579a8825
unknown <tristanroth00@gmail.com>
2024-12-16 11:18:54 +0100

    mise a jour avec view logs

--- moteurJeu/src/laby/views/ViewLogs.java
+++ moteurJeu/src/laby/views/ViewLogs.java
@@ -1,19 +1,32 @@
 package laby.views;
 
+import entites.enemies.Ennemy;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import laby.ModeleLabyrinth;
 import laby.Observer;
 import laby.Subject;
 
 public class ViewLogs implements Observer {
     private ModeleLabyrinth laby;
+    private VBox logs;
 
-    public ViewLogs(ModeleLabyrinth laby) {
+    public ViewLogs(ModeleLabyrinth laby, VBox logs) {
         this.laby = laby;
+        this.logs = logs;
     }
 
     @Override
     public void update(Subject s) {
-        ModeleLabyrinth laby = (ModeleLabyrinth) s;
-        System.out.println("Labyrinth updated");
+        if (laby.getLogs()==true){
+            logs.getChildren().clear();
+
+            for (Ennemy e : laby.deadEnemies){
+                Label l = new Label("Ennemy " + e + " is dead");
+                logs.getChildren().add(l);
+            }
+
+            laby.setLogs(false);
+        }
     }
 }
