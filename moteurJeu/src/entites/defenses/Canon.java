e5d52c7895b60dbbeffc811c86997d7da5fe7b1a
Marin <bourdonmarin@gmail.com>
2024-12-17 10:47:15 +0100

    Ajout des méthodes d'attaques des active/passive defenses (lorsqu'un ennemi entre dans la range)

--- moteurJeu/src/entites/defenses/Canon.java
+++ moteurJeu/src/entites/defenses/Canon.java
@@ -3,15 +3,7 @@ package entites.defenses;
 import entites.enemies.Ennemy;
 
 public class Canon extends ActiveDefense {
-
     public Canon(double x, double y) {
         super(x, y, 100, 10, 2.5, 1);
-    }
-
-    @Override
-    public void attack(Ennemy target) {
-        if (target != null) {
-            target.takeDamage(getDamages()*getBonus(getType(), target.getType()));
-        }
     }
 }
--- moteurJeu/src/entites/defenses/PassiveDefense.java
+++ moteurJeu/src/entites/defenses/PassiveDefense.java
@@ -4,11 +4,20 @@ import entites.enemies.Ennemy;
 
 public abstract class PassiveDefense extends Defense {
 
-        public PassiveDefense(double x, double y, int damage, double range) {
+    public PassiveDefense(double x, double y, int damage, double range) {
             super(x, y, damage, range);
         }
 
-        public void takeDamage(double damage) {
-
+    @Override
+    public void attack(Ennemy target) {
+        // On détecte si un ennemi est dans la portée de la défense avec une formule de distance euclidienne
+        if (((target.getX() - this.getX()) * (target.getX() - this.getX()) +
+                (target.getY() - this.getY()) * (target.getY() - this.getY()) <= this.getRange() * this.getRange())) {
+            target.takeDamage(getDamages()*getBonus(getType(), target.getType()));
         }
+        // C'est une défense passive, donc après avoir attaqué, elle s'autodétruit
+        this.takeDamage(10000);
+    }
+    public void takeDamage(double damage) {
+    }
 }
