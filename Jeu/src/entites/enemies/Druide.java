b9190a05b47a8a4df60def0d3c2a294bfe48c475
Florian Bortolotti <flori@MSI>
2025-01-09 11:08:50 +0100

    refactor classes ennemis

--- Jeu/src/entites/enemies/Druide.java
+++ Jeu/src/entites/enemies/Druide.java
@@ -3,14 +3,14 @@ package entites.enemies;
 import steering_astar.Steering.Vector2D;
 
 public class Druide extends Ennemy {
+
     public Druide(Vector2D position, String name) {
-
-        super(position, 100, 3, 10, 1, 2.5, 1,name,"/druide.png", "Healer");
-        this.setHealth(this.getHealth() + (Math.random() - 0.5) * 20);
-        this.setSpeed(this.getSpeed() + (Math.random() - 0.5));
-        this.setDamages(this.getDamages() + (Math.random() - 0.5) * 2);
-        this.setAttackSpeed(this.getAttackSpeed() + (Math.random() - 0.5) * 0.2);
-
+        super(position,
+                100 + (Math.random() - 0.5) * 20,
+                3 + (Math.random() - 0.5),
+                10 + (Math.random() - 0.5) * 2,
+                1 + (Math.random() - 0.5) * 0.2,
+                2.5, 1,name,"/druide.png", "Healer");
     }
 }
 
