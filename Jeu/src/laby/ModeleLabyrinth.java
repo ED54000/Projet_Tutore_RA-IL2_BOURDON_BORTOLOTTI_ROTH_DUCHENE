55aedee3e56b4a5faf117b5858ae0eafebc6807a
niram <marin.bourdon2@etu.univ-lorraine.fr>
2025-01-09 15:13:58 +0100

    fix survivalTime

--- Jeu/src/laby/ModeleLabyrinth.java
+++ Jeu/src/laby/ModeleLabyrinth.java
@@ -389,6 +389,8 @@ public class ModeleLabyrinth implements Jeu, Subject {
                         ennemi.attack(defense);
                         // le berserker se suicide après avoir attaqué
                         ennemi.takeDamage(1000);
+                        // On met à jour le temps de survie
+                        ennemi.setSurvivalTime(System.currentTimeMillis() - startTime);
                     }
                 }
             }
@@ -423,6 +425,8 @@ public class ModeleLabyrinth implements Jeu, Subject {
                             ennemyTarget.setKillerType(defense.getType());
                             // On retire la cible de la défense
                             ((ActiveDefense) defense).setTarget(null);
+                            // On met à jour le temps de survie de l'ennemi
+                            ennemyTarget.setSurvivalTime(System.currentTimeMillis() - startTime);
                         }
                     }
                     // Si l'ennemi n'est plus dans la portée de la défense
